eb1e0f89bb3dca3b28518f2acf15ee3bc74b2f8e
Brydon <brydon66@hotmail.ca>
2025-08-04 19:03:07 -0400

    declared arrays images

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -1,3 +1,12 @@
+/*
+	Name: Brydon Burrell
+	File: gallery-start/main.js
+	Date: 2025/08/01
+  This is a image gallery example
+*/
+
+
+// variable declarations
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -5,7 +14,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
