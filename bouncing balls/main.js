7830349fdcec3191f5d50c6ca6803ff1471443a4
Brydon <brydon66@hotmail.ca>
2025-08-04 19:34:11 -0400

    added helper functions

--- bouncing balls/main.js
+++ bouncing balls/main.js
@@ -15,14 +15,20 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+
+
 // function to generate random number
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
+// helper functions to generate random numbers and colors
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+  return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
 }
+
